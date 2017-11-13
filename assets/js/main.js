1aaa5e68029f42259cc4c1c652255b0a2d16e6ad
sarahgarcin <sarahgarcin@hotmail.com>
2017-11-13 11:04:27 +0100

    add night mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,21 +7,33 @@ $(document).ready(function(){
 
 function init(){
 
+	if($('body').attr('data-template') == 'home'){
+		const hours = new Date().getHours()
+		const isDayTime = hours > 6 && hours < 20
+		if(!isDayTime){
+			$('main').addClass('night-mode');
+		}
+		else{
+			$('main').removeClass('night-mode');
+		}
+	}
+
 	// open menu
 	$('header nav > ul > li').on('click', function(e){
 		var $el = $(this).find('ul');
 		var $allEl = $('header nav > ul > li > ul');
 		$('header nav > ul > li > a').removeClass('active');
-		$(this).children('a').addClass('active');
 		if($el.hasClass('active')){
 			$el.fadeOut(400);
 			$el.removeClass('active');
+			$(this).children('a').removeClass('active');
 		}
 		else{
 			$allEl.fadeOut(400);
 			$allEl.removeClass('active');
 			$(this).find('ul').fadeIn(400);
 			$el.addClass('active');
+			$(this).children('a').addClass('active');
 		}
 	})
 
